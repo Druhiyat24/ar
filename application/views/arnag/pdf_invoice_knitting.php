8b625e1dccd5b19e16a1c10a6a00c772f5100f87
Druhiyat24 <dederuhiyat24@gmail.com>
2026-03-18 11:06:33 +0700

    adjust

--- application/views/arnag/pdf_invoice_knitting.php
+++ application/views/arnag/pdf_invoice_knitting.php
@@ -175,15 +175,15 @@ table {
 			</tr>
 		<?php endforeach; ?>
 		<tr>
-			<td style="text-align: center" colspan='6'><b>Total Before Value Added Tax</b></td>
+			<td style="text-align: left" colspan='6'><b>Total Before Value Added Tax</b></td>
 			<td align='right'><b><?= $data_invoice_pot['twot']; ?></b></td>
 		</tr>
 		<tr>
-			<td style="text-align: center" colspan='6'><b>Value Added Tax</b></td>
+			<td style="text-align: left" colspan='6'><b>Value Added Tax</b></td>
 			<td align='right'><b><?= $data_invoice_pot['vat']; ?></b></td>
 		</tr>
 		<tr>
-			<td style="text-align: center" colspan='6'><b>Grand Total</b></td>
+			<td style="text-align: left" colspan='6'><b>Grand Total</b></td>
 			<td align='right'><b><?= $data_invoice_pot['grand_total']; ?></b></td>
 		</tr>
 
